ddbc8aa55d9a0641805709e884a68ef2d3dd5182
Yu Hao Ye <rxye1991@gmail.com>
2019-09-10 18:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neli deGrase Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic ='Let\'s talk about space.';
+var topic ='space.';
 
 var conversation = `${topic}`;
